cf0bdab6af0d6c38b39c251690806088ec4233aa
Peter Cosemans <peter.cosemans@gmail.com>
2023-01-14 23:11:43 +0100

    initial content

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# css-library-template
+# My csslib-simple
+
+A really small CSS framework
+
+### Get started
+
+```bash
+npm install csslib
+# or
+yarn add csslib
+# or 
+pnpm install ccslib
+```
+
---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import theme from "./docs/themes/github-dark.json";
+
+// https://astro.build/config
+export default defineConfig({
+  // add mx support
+  integrations: [mdx()],
+  srcDir: "./docs",
+
+  // setup syntax highlighting theme
+  markdown: {
+    shikiConfig: {
+      theme: {
+        type: "dark",
+        settings: theme.tokenColors,
+      },
+    },
+  },
+});
